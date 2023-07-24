7da9762799f48b72cbd135dc0e9e8281bf6d301c
cacko <alex@cacko.net>
2023-07-24 22:08:08 +0100

    fuckup.

--- ui/src/themes/index.js
+++ ui/src/themes/index.js
@@ -1,26 +1,5 @@
-import LightTheme from './light'
 import DarkTheme from './dark'
-import ExtraDarkTheme from './extradark'
-import GreenTheme from './green'
-import SpotifyTheme from './spotify'
-import LigeraTheme from './ligera'
-import MonokaiTheme from './monokai'
-import ElectricPurpleTheme from './electricPurple'
-import NordTheme from './nord'
-import GruvboxDarkTheme from './gruvboxDark'
 
 export default {
-  // Classic default themes
-  LightTheme,
   DarkTheme,
-
-  // New themes should be added here, in alphabetic order
-  ElectricPurpleTheme,
-  ExtraDarkTheme,
-  GreenTheme,
-  GruvboxDarkTheme,
-  LigeraTheme,
-  MonokaiTheme,
-  NordTheme,
-  SpotifyTheme,
 }
