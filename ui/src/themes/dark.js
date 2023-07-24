9813e92c3cb359a39f657848c3855edd51cc1f43
cacko <alex@cacko.net>
2023-07-24 23:04:15 +0100

    oops - thought I got that one.

--- ui/src/themes/dark.js
+++ ui/src/themes/dark.js
@@ -60,7 +60,7 @@ const musicListActions = {
 export default {
   themeName: 'Spotify-ish',
   typography: {
-    fontFamily: 'Share Tech Mono',
+    fontFamily: 'Syne Mono',
     h6: {
       fontSize: '1rem', // AppBar title
     },
